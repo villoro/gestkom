19ade5121ed64025415b72ad6e36dd420078eecc
Arnau Villoro <arnau@villoro.com>
2024-03-07 21:17:54 +0100

    F/blog layout (#6)
    * Remove blog summary + change image size + remove description and author + change post spacings
    
    * More images + more items per page + link also image
    
    * Dates in local format + hover post color
    
    * Remove PageHeader
    
    * Remove PostSidebar + add categories at top
    
    * Fill the void of PostSidebar

--- src/content/blog/post-4.md
+++ src/content/blog/post-4.md
@@ -3,8 +3,8 @@ title: "How to build an Application with modern Technology"
 meta_title: ""
 description: "this is meta description"
 date: 2022-04-04T05:00:00Z
-image: "/images/image-placeholder.png"
-categories: ["Architecture"]
+image: "/images/projects/5484.jpg"
+categories: ["Architecture", "Data"]
 author: "John Doe"
 tags: ["silicon", "technology"]
 draft: false
--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -1,11 +1,13 @@
 import { format } from "date-fns";
+import { ca, es } from 'date-fns/locale';
 
+// MMM for short month format, MMMM for full month name
 const dateFormat = (
   date: Date | string,
-  pattern: string = "dd MMM, yyyy",
+  pattern: string = "dd MMMM, yyyy",
 ): string => {
   const dateObj = new Date(date);
-  const output = format(dateObj, pattern);
+  const output = format(dateObj, pattern, {locale: ca});
   return output;
 };
 
