674eb7f48fa630fc2bb7beb32c0e0df0f66669d2
Arnau Villoro <arnau.villoro@northius.com>
2024-03-09 18:49:07 +0100

    Clean unusued tags functionality

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,6 @@ const blogCollection = defineCollection({
     image: z.string().optional(),
     author: z.string().default("Admin"),
     categories: z.array(z.string()).default(["others"]),
-    tags: z.array(z.string()).default(["others"]),
     draft: z.boolean().optional(),
   }),
 });
